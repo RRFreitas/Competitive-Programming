1c2c456da3f673601151d3ddf11035c0ebb6d5de
Rennan Rocha <rennanfreitas11@gmail.com>
2019-05-01 23:44:18 -0300

    Graphs

---
+++ Graphs/Dijkstra.py
@@ -0,0 +1,35 @@
+"""
+    Author: Rennan Rocha
+"""
+
+from heapq import *
+
+def dijkstra(node, fim):
+    q = []
+    
+    dist[node] = 0
+    heappush(q, (0, node))
+
+    while len(q) > 0:
+        top = heappop(q)
+
+        for vizinho in adj[top[1]]:
+            if dist[vizinho[1]] > dist[top[1]] + vizinho[0]:
+                dist[vizinho[1]] = dist[top[1]] + vizinho[0]
+                heappush(q, (dist[vizinho[1]], vizinho[1]))
+
+
+v, a = map(int, input().split())
+ini, fim = map(int, input().split())
+
+adj = [[] for i in range(v)]
+dist = [float("inf")] * v
+
+for i in range(a):
+    a, b, p = map(int, input().split())
+
+    adj[a].append((p, b))
+    adj[b].append((p, a))
+
+m = dijkstra(ini, fim)
+print(dist)
