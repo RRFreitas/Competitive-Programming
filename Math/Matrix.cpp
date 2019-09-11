456446e5763d8151e9fc42586d8bb7c0bcb6e43a
Rennan Rocha <rennanfreitas11@gmail.com>
2019-09-11 09:09:32 -0300

    Add files via upload

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-
 #define MOD 1000000007
 
 using namespace std;
@@ -15,66 +14,70 @@ ll add(ll a, ll b) {
 }
 
 struct Matrix {
-    int rows, cols;
-    int **mat;
+    ll **m, rows, cols;
 
     void init() {
-        mat = new int*[rows];
-        for(int i = 0; i < rows; i++)
-            mat[i] = new int[cols];
+        m = new ll*[rows];
+        for(ll i = 0; i < rows; i++)
+            m[i] = new ll[cols];
     }
+
     void clean() {
         for(int i = 0; i < rows; i++)
-            delete [] mat[i];
-        delete [] mat;
+            delete [] m[i];
+        delete [] m;
+    }
+    
+    void ident() {
+        for(int i = 0; i < min(rows,cols); i++)
+            m[i][i] = 1;
     }
 
     Matrix(int row, int col) : rows(row), cols(col) {
         init();
         for(int i = 0; i < rows; i++)
-            memset(mat[i], 0, cols * sizeof(int));
+            memset(m[i], 0, cols * sizeof(ll));
     }
 
     Matrix(const Matrix &o) : rows(o.rows), cols(o.cols) {
         init();
         for(int i = 0; i < rows; i++)
-            memcpy(mat[i], o.mat[i], cols * sizeof(int));
+            memcpy(m[i], o.m[i], cols * sizeof(ll));
     }
 
     Matrix& operator = (const Matrix &o) {
         clean();
-        rows = o.rows; cols = o.cols;
+        rows = o.rows;
+        cols = o.cols;
         init();
         for(int i = 0; i < rows; i++)
-            memcpy(mat[i], o.mat[i], cols * sizeof(int));
+            memcpy(m[i], o.m[i], cols * sizeof(ll));
         return *this;
     }
 
-    Matrix operator % (Matrix b) const {
-        assert(cols == b.rows);
-        Matrix c = {rows, b.cols};
-        int lin = rows;
-        int col = b.cols;
-        for(int i = 0; i < lin; i++) {
-            for(int j = 0; j < col; j++) {
+    Matrix operator % (Matrix o) const {
+        assert(cols == o.rows);
+        Matrix res = Matrix(rows, o.cols);
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < o.cols; j++) {
                 for(int k = 0; k < cols; k++) {
-                    c.mat[i][j] = add(c.mat[i][j], mul(mat[i][k], b.mat[k][j]));
+                    res.m[i][j] = add(res.m[i][j], mul(m[i][k], o.m[k][j]));
                 }
             }
         }
-        return c;
+        return res;
     }
 
     Matrix operator ^ (ll exp) const {
-        Matrix ret = Matrix(rows, cols), base = *this;
-        for(int i = 0; i < rows; i++) ret.mat[i][i] = 1;
+        Matrix res = Matrix(rows, cols), base = *this;
+        res.ident();
 
         while(exp) {
-            if(exp & 1) ret = ret % base;
+            if(exp & 1) res = res % base;
             exp >>= 1;
             if(exp) base = base % base;
         }
-        return ret;
+        return res;
     }
 
     ~Matrix() {clean();}
