ce0bf12dee7cf58c934d9c639ba1f11b58354918
Rennan Rocha <rennanfreitas11@gmail.com>
2021-08-22 08:27:59 -0300

    LCP

--- Strings/Suffix_Array.cpp
+++ Strings/Suffix_Array.cpp
@@ -48,10 +48,31 @@ vector<int> SA(string s) {
     return p;
 }
 
+vector<int> kasai(vector<int> sa, string s){
+    int n = sa.size();
+    vector<int> r(n), lcp(n);
+    for (int i=0; i<n; i++) r[sa[i]] = i;
+
+    int h=0;
+    for (int i=0; i<n; i++){
+        if (r[i] == n-1) h = lcp[n-1] = 0;
+        else{
+            int j = sa[r[i] + 1];
+            while(i + h < n && j + h < n && s[i+h] == s[j+h]) h++;
+            lcp[r[i]] = h;
+            h = max(0, h-1);
+        }
+    }
+    return lcp;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     string s;
     cin >> s;
     vector<int> suf = SA(s);
     for(int i = 1; i < suf.size(); i++) cout << suf[i] << "\n";
+    cout << "\n";
+    vector<int> lcp = kasai(suf, s);
+    for(int x : lcp) cout << x << "\n";
 }
