f59019d7189e1b7ad83f8bfb540529c671729a90
Rennan Rocha <rennanfreitas11@gmail.com>
2021-10-23 07:44:53 -0300

    struct bit

--- Tree/BITree.cpp
+++ Tree/BITree.cpp
@@ -1,44 +1,29 @@
-#include <bits/stdc++.h>
+// tree is 1-indexed, base is 0-indexed
+struct BIT{
+    int size;
+    vector<ll> tree;
+    vector<ll> base;
 
-#define debugarr(a) for(int i : a) cout << i << " ";
-#define N 8
-#define LSB(i) ((i) & -(i))
-
-// LSB = Less Significant Bit
-
-using namespace std;
-
-int bit[N+1]; // 1..N indexed
-int arr[N] = {5,2,3,8,9,1,2,3};
-
-int getSum(int index) {
-    return index == 0 ? 0 : bit[index] + getSum(index - LSB(index));
-}
-
-void update(int i, int val) {
-    if(i > N) return;
-    bit[i] += val;
-    update(i + LSB(i), val);
-}
-
-int query(int begin, int end) {
-    return getSum(end) - getSum(begin - 1);
-}
-
-void buildBIT() {
-    for(int i = 0; i < N; i++) {
-        update(i+1, arr[i]);
+    BIT(vector<ll> &base) : base(base) {
+        size = base.size();
+        tree.resize(size+1);
+        for(int i = 1; i <= size; i++)
+            update(i, base[i-1]);
     }
-}
 
-int main() {
-    buildBIT(); 
-
-    debugarr(bit);
-    cout << endl;
-
-    for(int i = 1; i < 100; i++) {
-        update(1, i);
-        cout << query(1, N) << endl;
+    void update(int i, ll val) {
+        for(; i <= size; i += (i&-i))
+            tree[i] += val;
     }
-}
+
+    ll getSum(int i) {
+        ll ans = 0;
+        for(; i > 0; i -= (i&-i))
+            ans += tree[i];
+        return ans;
+    }
+
+    ll query(int l, int r) {
+        return getSum(r) - getSum(l-1);
+    }
+};
